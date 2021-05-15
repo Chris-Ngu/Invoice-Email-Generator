96792de7c5e86652b782d8ce778c7d9b79b96907
Christopher Nguyen <Chris_ngu@pm.me>
2021-05-15 11:36:49 -0500

    adding controller support for calculations

--- src/main/java/com/example/demo/InvoiceController.java
+++ src/main/java/com/example/demo/InvoiceController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,27 @@ public class InvoiceController {
 
     @GetMapping()
     public String Invoice(Model model) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        
+        Item[] items = {
+            new Item("Keyboard", 10.0, 1),
+            new Item("Headphone", 12.0, 2),};
+
+        String taxRate = "8.6";
+        double subTotal = 0.0;
+        double taxAmount = 0.0;
+        double total = 0.0;
+
+        for (Item x : items) {
+            subTotal += x.getItemTotal();
+        }
+
+        // Converting tax rate to decimal 
+        total = Double.parseDouble(df.format(subTotal * (1 + (Double.parseDouble(taxRate) / 100))));
+        taxAmount = Double.parseDouble(df.format(total - subTotal));
+        
+        
+        
         model.addAttribute("vendor", new Vendor(
                 "John Doe",
                 "8 Elm Street",
@@ -24,13 +46,13 @@ public class InvoiceController {
                 "Due on receipt",
                 "28/07/2016"
         ));
-        model.addAttribute("item", Arrays.asList(
-                new Item("Keyboard", 10.0, 1),
-                new Item("Headphone", 12.0, 2)
-        ));
+        model.addAttribute("item", items);
         model.addAttribute("billTo", "janedoe@email.com");
         model.addAttribute("note", "Thanks for your business!");
-        model.addAttribute("taxRate", "8.6");
+        model.addAttribute("taxRate", taxRate);
+        model.addAttribute("subTotal", subTotal);
+        model.addAttribute("total", total);
+        model.addAttribute("taxAmount", taxAmount);
         return "Invoice";
     }
 }
