3c7a6fb6c11e4cebea25ab19a99e486e1ce63048
Christopher Nguyen <Chris_ngu@pm.me>
2021-05-28 16:58:50 -0500

    disabling JPA, moving to backend folder

---
+++ backend/src/main/java/com/example/demo/restAPI/models/Item.java
@@ -0,0 +1,50 @@
+//package com.example.demo.restAPI.models;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class Item {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Integer id;
+//    private String name;
+//    private Integer quantity;
+//    private Double itemTotal;
+//
+//    // Constructor here?
+//    public Integer getId() {
+//        return this.id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return this.name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Integer getQuantity() {
+//        return this.quantity;
+//    }
+//
+//    public void setQuantity(Integer quantity) {
+//        this.quantity = quantity;
+//    }
+//
+//    public Double getItemTotal() {
+//        return this.itemTotal;
+//    }
+//
+//    public void setItemTotal(Double itemTotal) {
+//        this.itemTotal = itemTotal;
+//    }
+//}
