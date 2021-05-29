a326894f1b5d4c7bffbb9e857ce85420cf9b5322
Christopher Nguyen <Chris_ngu@pm.me>
2021-05-29 18:08:06 -0500

    adding angular forms for API calls

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from "@angular/forms";
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title: string = 'frontend';
+
+  // Vendor form information
+  name = new FormControl("");
+  street = new FormControl("");
+  province = new FormControl("");
+  country = new FormControl("");
+  phone = new FormControl("");
+  email = new FormControl("");
+
+  // Invoice Information
+  invoiceNumber = new FormControl("");
+  dateCreated = new Date().toUTCString();
+  notes = new FormControl("");
+  dueDate = new NgbDate(1, 1, 1999);
+
+  onDueDateSelect = (date: NgbDate): void => {
+    this.dueDate = new NgbDate(date.year, date.month, date.day);
+    // alert(`Date: ${date.day}, Month: ${date.month}, Year: ${date.year}`);
+  }
 }
