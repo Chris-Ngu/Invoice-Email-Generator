28b05f88c3ae5dca8b78c4a0dbbff91c8c3ded1e
Christopher Nguyen <Chris_ngu@pm.me>
2021-05-30 21:55:37 -0500

    fixing invoice add/ remove index issue

--- frontend/src/app/invoice-plus-button/invoice-plus-button.component.ts
+++ frontend/src/app/invoice-plus-button/invoice-plus-button.component.ts
@@ -1,5 +1,6 @@
 // ref: https://stackblitz.com/edit/add-or-remove-dynamic-component?file=src%2Fapp%2Fparent%2Fparent.component.ts
-import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ComponentRef, ComponentFactoryResolver, ViewChild, ViewContainerRef, ViewRef } from '@angular/core';
+import { ItemPanelComponent } from "../item-panel/item-panel.component";
 
 @Component({
   selector: 'app-invoice-plus-button',
@@ -8,12 +9,33 @@ import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef } from
 })
 export class InvoicePlusButtonComponent {
 
-  @ViewChild("viewContainerRef", { read: ViewContainerRef })
-  VCR: ViewContainerRef;
+  @ViewChild("viewContrainerRef", { read: ViewContainerRef })
+  VCR!: ViewContainerRef;
 
+  childInvoiceKey: number = 0;
+  childInvoiceComponents = Array<ComponentRef<ItemPanelComponent>>();
+
+  constructor(private CFR: ComponentFactoryResolver) { }
 
   createInvoiceItem = (): void => {
+    let componentFactory = this.CFR.resolveComponentFactory(ItemPanelComponent);
+    let invoicePanelRef = this.VCR.createComponent(componentFactory);
+    let invoicePanel = invoicePanelRef.instance;
+    invoicePanel.key = ++this.childInvoiceKey;
+    invoicePanel.parentRef = this;
 
+    this.childInvoiceComponents.push(invoicePanelRef);
+  }
+
+  removeInvoiceItem = (key: number): void => {
+    if (this.VCR.length < 1) return;
+    let invoiceRef = this.childInvoiceComponents.filter(x => x.instance.key === key)[0];
+
+    // Not sure why this is returning -1, I guess it can't find it for some reason
+    // let vcrIndex: number = this.VCR.indexOf(invoiceRef as any);
+    let vcrIndex = this.childInvoiceComponents.indexOf(invoiceRef);
+    this.VCR.remove(vcrIndex);
+    this.childInvoiceComponents = this.childInvoiceComponents.filter(x => x.instance.key !== key);
   }
 
 }
--- frontend/src/app/item-panel/item-panel.component.ts
+++ frontend/src/app/item-panel/item-panel.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentRef } from '@angular/core';
+import { InvoicePlusButtonComponent } from "../invoice-plus-button/invoice-plus-button.component";
 
 @Component({
   selector: 'app-item-panel',
   templateUrl: './item-panel.component.html',
   styleUrls: ['./item-panel.component.css']
 })
-export class ItemPanelComponent implements OnInit {
+export class ItemPanelComponent {
 
-  constructor() { }
+  public key!: number;
+  public parentRef!: InvoicePlusButtonComponent;
 
-  ngOnInit(): void {
+  constructor() {
+  }
+
+  removeInvoiceItem = (): void => {
+    this.parentRef.removeInvoiceItem(this.key);
   }
 
 }
