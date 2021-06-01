ced50daeb6815fa5d885680aa27c066902a219e7
Christopher Nguyen <Chris_ngu@pm.me>
2021-06-01 14:29:43 -0500

    adding onto POST REST request function

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component, ComponentRef } from '@angular/core';
 import { FormControl } from "@angular/forms";
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError, retry } from "rxjs/operators";
+
+import { InvoiceInfo } from "./models/invoice-info";
+import { Item } from "./models/item";
+import { Vendor } from "./models/vendor";
+import { Invoice } from "./models/invoice";
 
 import { ItemPanelComponent } from "./item-panel/item-panel.component";
 
@@ -24,15 +32,18 @@ export class AppComponent {
   invoiceNumber = new FormControl("");
   dateCreated = new Date().toUTCString();
   notes = new FormControl("");
-  dueDate = new NgbDate(1, 1, 1999);
+  dueDate = new Date().toUTCString();
 
   //Invoicedata
   childInvoicesRef = Array<ComponentRef<ItemPanelComponent>>();
+  childInvoices: Item[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   public onDueDateSelect = (date: NgbDate): void => {
-    this.dueDate = new NgbDate(date.year, date.month, date.day);
+    // this.dueDate = new NgbDate(date.year, date.month, date.day);
+    this.dueDate = new Date(date.year, date.month, date.day).toUTCString();
+
     // alert(`Date: ${date.day}, Month: ${date.month}, Year: ${date.year}`);
   }
 
@@ -47,6 +58,30 @@ export class AppComponent {
     // https://stackoverflow.com/questions/58378294/posting-a-nested-object-from-angular-to-spring-rest-api-is-always-null
     // REST call to Spring backend
     // Need to test with basic response first
+    const vendorInfo: Vendor = new Vendor(
+      this.name.value,
+      this.street.value,
+      this.province.value,
+      this.country.value,
+      this.phone.value,
+      this.email.value);
+    const invoiceInfo: InvoiceInfo = new InvoiceInfo(
+      this.invoiceNumber.value,
+      this.notes.value,
+      this.dateCreated,
+      this.dueDate);
+
+    // converting itemRefs to items
+    this.childInvoicesRef.forEach((invoiceRef: ComponentRef<ItemPanelComponent>) => {
+      this.childInvoices.push(new Item(
+        invoiceRef.instance.itemName.value,
+        invoiceRef.instance.itemCost.value,
+        invoiceRef.instance.quantity.value,
+        invoiceRef.instance.itemTotal.value));
+    })
+    const headers = { "content-type": "application/json" };
+    // https://www.tektutorialshub.com/angular/angular-http-post-example/#model
+    // Call POST request here
 
   }
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { InvoicePlusButtonComponent } from './invoice-plus-button/invoice-plus-b
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     NgbModule,
     ReactiveFormsModule,
